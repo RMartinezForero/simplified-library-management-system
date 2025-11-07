67e1ce17ada100cfed93f1d58c73401f7fa891c5
Ricardo Martínez <ricardo300302@gmail.com>
2025-11-07 10:48:33 -0500

    feat: create LibraryService interface with book management methods

---
+++ src/main/java/com/ricardo/simplified_library_management_system/service/LibraryService.java
@@ -0,0 +1,13 @@
+package com.ricardo.simplified_library_management_system.service;
+
+import java.util.List;
+import com.ricardo.simplified_library_management_system.model.Book;
+
+public interface LibraryService {
+    void addBook(Book newBook);
+    List<Book> getAllBooks();
+    Book getBookById(Long id);
+    void updateBookData(Long id, Book book);
+    void deleteBookById(Long id);
+    List<Book> getBooksWithSubstring(String text);
+}
