0e3c2829c9a7c407a081c2f239fb2c877cd5a210
Ricardo Martínez <ricardo300302@gmail.com>
2025-11-07 09:59:21 -0500

    first commit

---
+++ src/main/java/com/ricardo/simplified_library_management_system/SimplifiedLibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.simplified_library_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedLibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedLibraryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/simplified_library_management_system/SimplifiedLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.simplified_library_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedLibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
