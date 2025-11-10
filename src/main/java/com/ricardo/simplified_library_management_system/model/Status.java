3599cd848e893d8248b916575fe9f6f925390976
Ricardo Martínez <ricardo300302@gmail.com>
2025-11-10 10:38:20 -0500

    feat: enhance addBook() and getBookById() methods with validation checks

--- src/main/java/com/ricardo/simplified_library_management_system/model/Status.java
+++
@@ -1,5 +0,0 @@
-package com.ricardo.simplified_library_management_system.model;
-
-public enum Status {
-    AVAILABLE, BORROWED;
-}
--- src/main/java/com/ricardo/simplified_library_management_system/service/LibraryServiceImpl.java
+++ src/main/java/com/ricardo/simplified_library_management_system/service/LibraryServiceImpl.java
@@ -1,15 +1,20 @@
 package com.ricardo.simplified_library_management_system.service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.ricardo.simplified_library_management_system.excepcion.BookNotFoundException;
 import com.ricardo.simplified_library_management_system.model.Book;
+import com.ricardo.simplified_library_management_system.model.BookStatus;
+
+import jdk.jshell.Snippet.Status;
 
 @Service
 public class LibraryServiceImpl implements LibraryService {
-
+    // TODO: AtomicLong para id?
     private List<Book> books;
 
     public LibraryServiceImpl() {
@@ -18,6 +23,28 @@ public class LibraryServiceImpl implements LibraryService {
 
     @Override
     public void addBook(Book newBook) {
+        // TODO: validar correctamente para ID basado en AtomicLong
+        if (newBook == null) {
+            throw new IllegalArgumentException("The Book object can´t be null.");
+        }
+        if (newBook.getAuthor().isBlank() || newBook.getAuthor() == null) {
+            throw new IllegalArgumentException("Author's name is missing or blank.");
+        }
+        if (newBook.getGenre().isBlank() || newBook.getGenre() == null) {
+            throw new IllegalArgumentException("Genra data is missing or blank.");
+        }
+        if (newBook.getId() <= 0) {
+            throw new IllegalArgumentException("Id most be positive.");
+        }
+        if (newBook.getIsbn().isBlank() || newBook.getIsbn() == null) {
+            throw new IllegalArgumentException("ISBN data is missing or blank.");
+        }
+        if (newBook.getPublicationDate().isAfter(LocalDate.now()) || newBook.getPublicationDate() == null) {
+            throw new IllegalArgumentException("Publication date is missing or is later than today.");
+        }
+        if (newBook.getTitle().isBlank() || newBook.getAuthor() == null) {
+            throw new IllegalArgumentException("Title is missing or blank.");
+        }
         books.add(newBook);
     }
 
@@ -28,10 +55,18 @@ public class LibraryServiceImpl implements LibraryService {
 
     @Override
     public Book getBookById(Long id) {
+        // TODO: validar AtomicLong
+        if (id == null) {
+            throw new IllegalArgumentException("Id is missing.");
+        }
+        if (id < 0) {
+            throw new IllegalArgumentException("Id most be positive.");
+        }
+
         return books.stream()
                 .filter(b -> b.getId().equals(id))
                 .findFirst()
-                .orElse(null);
+                .orElseThrow(() -> new BookNotFoundException("The book does not exist."));
     }
 
     @Override
@@ -46,6 +81,7 @@ public class LibraryServiceImpl implements LibraryService {
 
     @Override
     public List<Book> getBooksWithSubstring(String text) {
+        // TODO: validar a text
         return null;
     }
 
