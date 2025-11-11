45cd3510c5b4c22b3549c4fbf5eef98bf23ccbd7
Ricardo Martínez <ricardo300302@gmail.com>
2025-11-11 11:06:02 -0500

    refactor: remove unused import statement for Autowired in LibraryController

--- src/main/java/com/ricardo/simplified_library_management_system/controller/LibraryController.java
+++ src/main/java/com/ricardo/simplified_library_management_system/controller/LibraryController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
